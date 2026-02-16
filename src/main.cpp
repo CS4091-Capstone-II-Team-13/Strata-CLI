2fc2750aad89778fb9d9214eb2eedc113d2f3bdb
connorWinningUM <cew8t4@umsystem.edu>
2026-02-16 16:04:29 -0600

    Updated Parser design

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 /*
-/   Date: 2/11/26
+/   Date: 2/16/26
 /   Ver:  1.0.0
 /
-/   Purpose:    This main.cpp file handles all of the std input and output of the command line interface.
-/               All commands and flags will be handled in their respective header/src files.
+/   Purpose:    This main.cpp file handles all of the high-level application logic
 /               
 */
 
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void Parser::parse() {
     parseFlag();
     parseCommand();
+    parseFlag();
 
     // the remainder of the arguments will be passed forward
     args = deque<string>(inputs);
